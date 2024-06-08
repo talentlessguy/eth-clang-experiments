44a843279e14e72332d86e9dc8ba6c1a16da692c
v1rtl <hi@v1rtl.site>
2024-06-08 14:01:55 +0300

    encode addr(bytes32)

--- scripts/ens.ts
+++ scripts/ens.ts
@@ -1,4 +1,30 @@
 import { labelhash, namehash } from 'https://esm.sh/viem@2.13.3/ens'
+import { encodeFunctionData } from 'https://esm.sh/viem@2.13.3'
 
 console.log(`labelhash: ${labelhash('eth')}`)
-console.log(`namehash: ${namehash('v1rtl.eth')}`)
+const node = namehash('v1rtl.eth')
+console.log(`namehash: ${node}`)
+
+console.log(`Encoded abi: ${encodeFunctionData({
+    functionName: 'addr',
+    abi: [{
+        "inputs": [
+            {
+                "internalType": "bytes32",
+                "name": "node",
+                "type": "bytes32"
+            }
+        ],
+        "name": "addr",
+        "outputs": [
+            {
+                "internalType": "address payable",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },],
+    args: [node]
+})}`)
--- src/ens.c
+++ src/ens.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
+#include <string.h>
 #include <ethc/keccak256.h>
 #include <ethc/hex.h>
-#include <string.h>
+#include <ethc/abi.h>
 
 char *labelhash(const char *label)
 {
     uint8_t result[32] = {0};
     char *hash = NULL;
 
-    // If the label is empty, return the zero-filled hash
     if (!label || strlen(label) == 0)
     {
         eth_hex_from_bytes(&hash, result, 32);
         return hash;
     }
 
-    // Check if the label is in the encoded form [hash]
     if (strlen(label) == 66 && label[0] == '[' && label[65] == ']')
     {
         char encoded_hash[65];
@@ -25,9 +23,8 @@ char *labelhash(const char *label)
         encoded_hash[64] = '\0';
 
         uint8_t *hash_bytes = NULL;
-        if (eth_hex_to_bytes(&hash_bytes, encoded_hash, 64) == 0)
+        if (1 == eth_hex_to_bytes(&hash_bytes, encoded_hash, 64))
         {
-            // If the conversion to bytes is successful
             hash = (char *)malloc(67);
             sprintf(hash, "0x%s", encoded_hash);
             free(hash_bytes);
@@ -49,9 +46,9 @@ char *labelhash(const char *label)
 
 char *namehash(const char *name)
 {
-    uint8_t result[32] = {0}; // Initialize result as 32 bytes of zero
+    uint8_t result[32] = {0};
     char *final_hash = NULL;
-    char *labels_copy = strdup(name); // Make a copy of the input name to tokenize
+    char *labels_copy = strdup(name);
     char *token;
     char *labels[256]; // Array to hold labels, assuming a maximum of 256 labels
     int label_count = 0;
@@ -85,7 +82,6 @@ char *namehash(const char *name)
         free(hash);
     }
 
-    // Convert final result to hexadecimal string
     eth_hex_from_bytes(&final_hash, result, 32);
     free(labels_copy);
     return final_hash;
@@ -93,9 +89,21 @@ char *namehash(const char *name)
 
 int main()
 {
+    size_t hexlen;
     const char *name = "v1rtl.eth";
     char *hash = namehash(name);
     printf("namehash: 0x%s\n", hash);
-    free(hash);
+
+    struct eth_abi abi;
+    char *fn = "addr(bytes32)", *hex;
+    eth_abi_init(&abi, ETH_ABI_ENCODE);
+    eth_abi_call(&abi, &fn, NULL); // balanceOf(
+    eth_abi_bytes32(&abi, &hash);  //   0x6dd56164f699a101d6063add452dfed7c6c09fe17b8e4acf3328f9387f5030b9
+    eth_abi_call_end(&abi);        // )
+
+    eth_abi_to_hex(&abi, &hex, &hexlen);
+    eth_abi_free(&abi);
+    printf("encoded abi: %s\n", hex); // 3b3b57dee0920cc7aa5a000000000000000000000051f908b2afa579f053c63afe7f0000 abi: 70a08231000000000000000000000000876d477bd5cd050e6162cf757e1bc02d93cdc0fe
+    free(hex);
     return 0;
 }
